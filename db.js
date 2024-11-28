fcbbd4bf2eb98552f006daf88b81568ac999c02f
Tharika Shree R <tharikashreer@gmail.com>
2024-11-28 17:42:30 +0530

    Modified Domain

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: "school-api-black.vercel.app",
+    host: "school-api-black",
     user: "root",
     password: process.env.DB_PASSWORD,
     database: "school-api",
